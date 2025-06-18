b1c285e565c409b99f81b83723ee6f03838eb91a
Mad-Joy <119327864+Mad-Joy@users.noreply.github.com>
2025-06-18 12:14:22 -0300

    Update completion_preview.lua

--- lua/supermaven-nvim/completion_preview.lua
+++ lua/supermaven-nvim/completion_preview.lua
@@ -1,6 +1,6 @@
 
 local u = require("supermaven-nvim.util")
-local mj_buf_set_extmark = require('floating_ghosts.floating_ghosts').mj_buf_set_extmark
+local mj_buf_set_extmark = require('floating-ghosts.floating_ghosts').mj_buf_set_extmark
 
 local CompletionPreview = {
   inlay_instance = nil,
